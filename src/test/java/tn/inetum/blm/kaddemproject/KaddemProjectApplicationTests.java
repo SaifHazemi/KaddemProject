2f8eb0c699a5c2e229a92a7b9a85244a5a7a9cb3
Saif-Eddine Hazemi <saif-eddine.hazemi@inetum.com>
2023-02-26 18:35:04 +0100

    First commit

---
+++ src/test/java/tn/inetum/blm/kaddemproject/KaddemProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.inetum.blm.kaddemproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaddemProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
